7e2323327a0a709848e46598308b45992d56c9fc
Danilo Rodrigues <devdanrodrigues@gmail.com>
2026-04-08 12:10:24 -0300

    Update ex3.py

--- Python/ex3.py
+++ Python/ex3.py
@@ -1,4 +1,4 @@
 n1=int(input('Digite o primeiro número '))
 n2=int(input('Digite o segundo número '))
 s = n1+n2
-print('A soma entre os números é {}'.format(s))
+print('A soma entre os números {} e {} é {}'.format(n1,n2,s))
